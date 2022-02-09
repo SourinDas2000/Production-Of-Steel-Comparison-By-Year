a58d4f2a240547ad017cd3ab2e97212a56e47660
SourinDas2000 <99353367+SourinDas2000@users.noreply.github.com>
2022-02-09 22:56:48 +0530

    Update Production Of Steel Comparison By Year.py

--- Production Of Steel Comparison By Year.py
+++ Production Of Steel Comparison By Year.py
@@ -1,7 +1,4 @@
 
-# This program reads an excel sheet data of steel production of a particular company from the year 1990 to 1996. The program plots the data in a line chart.
-
-
 import matplotlib.pyplot as plt
 
 import pandas as pd
